7641d073d2c77827ad00997690a567d2cc6db644
wangzhenxi <im@joshwong.cn>
2020-12-20 18:54:48 +0800

    :sparkles: 支持jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,20 @@
 module.exports = {
   root: true,
   extends: ['airbnb-standard', 'prettier'],
+  overrides: [
+    {
+      files: ['packages/*/test/**/*.js', 'test/**/*.js'],
+      env: {
+        jest: true,
+      },
+      plugins: ['jest'],
+      rules: {
+        'jest/no-disabled-tests': 'warn',
+        'jest/no-focused-tests': 'error',
+        'jest/no-identical-title': 'error',
+        'jest/prefer-to-have-length': 'warn',
+        'jest/valid-expect': 'error',
+      },
+    },
+  ],
 }
