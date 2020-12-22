220cf1be11627fdcfb6fd863ba8cd7f3bab69954
wangzhenxi <im@joshwong.cn>
2020-12-22 16:23:13 +0800

    :pencil2: 构建统一输出到lib

--- jest.config.js
+++ jest.config.js
@@ -152,7 +152,7 @@ module.exports = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/dist/'],
+  testPathIgnorePatterns: ['/node_modules/', '<rootDir>/lib/'],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   testRegex: [`/packages/[^/]+/test/.+\\.m?js`],
